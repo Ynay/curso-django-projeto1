8ca1b4f7ccbd1a5886d1376a2cd622205c61bf21
Ynay <[syan10613@gmail.com]>
2022-04-07 22:11:02 -0300

    create page home

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,9 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
 
 
-def sobre(request):
-    return HttpResponse('Sobre')
 
-
-def contato(request):
-    return HttpResponse('Contato')
