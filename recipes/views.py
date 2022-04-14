de2c0971fe4cf69744a283923b673aa68b20c96a
Ynay <[syan10613@gmail.com]>
2022-04-14 14:15:50 -0300

    Creating the urls and views

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Yan Santos',
+    })
 
 
-
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Yan Santos',
+    })
