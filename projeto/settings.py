0d6622e14efd5f5aeaf680d1521690ee485a3678
Ynay <[syan10613@gmail.com]>
2022-04-14 23:03:43 -0300

    Home page update

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,7 +55,12 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates', ],
+        'DIRS': [
+
+            BASE_DIR / 'base_templates',
+            BASE_DIR / 'templates',
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
